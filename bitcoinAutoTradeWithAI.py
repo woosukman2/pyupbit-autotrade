36a4b5b5ae3c6c010ccce02bc38b02306d8545ab
woosukman2 <47045116+woosukman2@users.noreply.github.com>
2024-02-16 11:37:40 +0900

    Update bitcoinAutoTradeWithAI.py
    K = 0.8

--- bitcoinAutoTradeWithAI.py
+++ bitcoinAutoTradeWithAI.py
@@ -69,7 +69,7 @@ while True:
 
         #9:00 < 현재 < #8:59 :50
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.7)
+            target_price = get_target_price("KRW-BTC", 0.8)
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
