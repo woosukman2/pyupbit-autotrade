d8d44af12683502fda2cb49a2fda7cd47980c5c0
karrigen <47045116+woosukman2@users.noreply.github.com>
2024-12-03 11:27:19 +0900

    Update btc.py

--- btc.py
+++ btc.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "your-access"
-secret = "your-secret"
+access = "cdIGOvAcKnGW3JZuHb97tc0iexBuclpkdJ3saqHB"
+secret = "dQFkGunoGpELbYU5ZjOgdi4sI2S9dvjVFrQLY8Av"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
